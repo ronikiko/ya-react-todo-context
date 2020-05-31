42e93101f782725cf6ac60d752aa2812344bcceb
ronikiko <ronikiko@gmail.com>
2020-06-01 00:13:23 +0300

    h

--- src/components/CompletedTodos.js
+++ src/components/CompletedTodos.js
@@ -4,32 +4,38 @@ import todoContext from "../context/TodoContext";
 function CompletedTodos() {
   const context = useContext(todoContext);
 
-  const comp = context.todo.filter((todo) => todo.completed === true);
-
-  return (
-    <div>
-      <h1>Completed Todos</h1>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Due Date</th>
-            <th>Completed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {comp &&
-            comp.map((to, index) => (
-              <tr key={index}>
-                <td>{to.title}</td>
-                <td>{to.doeDate}</td>
-                <td>{to.completed ? "true" : ""}</td>
+  const completed = () => {
+    const comp = context.todo.filter((todo) => todo.completed === true);
+    if (comp.length > 0) {
+      return (
+        <div>
+          <h1>Completed Todos</h1>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Due Date</th>
+                <th>Completed</th>
               </tr>
-            ))}
-        </tbody>
-      </table>
-    </div>
-  );
+            </thead>
+            <tbody>
+              {comp &&
+                comp.map((to, index) => (
+                  <tr key={index}>
+                    <td>{to.title}</td>
+                    <td>{to.doeDate}</td>
+                    <td>{to.completed ? "true" : ""}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>No Completed Todos</h1>;
+    }
+  };
+  return <div>{completed()}</div>;
 }
 
 export default CompletedTodos;
